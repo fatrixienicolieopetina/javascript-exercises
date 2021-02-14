80be7af4e0ff7ad9eefcc2102b5d104115b88dbe
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-02-14 22:25:21 +0800

    Fix tests for helloWorld, repeatString, reverseString, caesar exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, numOfTimes) {
+    if(numOfTimes < 0) {
+        return 'ERROR';
+    }
 
+    let repeatedString = '';
+    while(numOfTimes > 0) {
+        repeatedString = repeatedString.concat(str);
+        numOfTimes--;
+    }
+
+    return repeatedString;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedStr = '';
 
+    for(let i = str.length; i >= 0; --i) {
+        reversedStr = reversedStr.concat(str.charAt(i));
+    }
+
+    return reversedStr;
 }
 
 module.exports = reverseString
