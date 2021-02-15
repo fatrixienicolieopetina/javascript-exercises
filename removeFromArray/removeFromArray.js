d98d0e5c0763c89a214315f276170e3c4b938ba9
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-02-15 23:06:18 +0800

    Fixed tests for leapYear, removeFromArray, sumAll and tempConversion

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for(let i = args.length; i >= 0; --i) {
+        let elementIndex = array.indexOf(args[i]);
+        while(elementIndex > -1) {
+            array.splice(elementIndex, 1);
+            elementIndex = array.indexOf(args[i]);
+        }
+    }
 
+    return array;
 }
 
 module.exports = removeFromArray
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if(Number.isInteger(num1) && Number.isInteger(num2)) {
+
+        if(num1 < 0 || num2 < 0) {
+            return 'ERROR';
+        }
+
+        let sum = 0;
+        let i = (num1 <= num2) ? num1: num2;
+        let j = (num1 <= num2) ? num2: num1;
+
+        for(let k = i; k <= j; k++) {
+            sum = sum + k;
+        }
+
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 
 }
 
