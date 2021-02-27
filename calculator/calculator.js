09ea3a3343ba1ebfdcc5256fc1029da8b218a57e
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-02-27 15:58:42 +0800

    Added solution for calculator exercise

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,30 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	return arr.reduce((sum, item) => sum + item, 0);
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	return arr.reduce((product, num) => product * num, 1);
 }
 
-function power() {
-	
+function power(base, power) {
+	return Math.pow(base, power);	
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let factorialNum = 1;
+	for(let i = 2; i <= num; i++) {
+		factorialNum = factorialNum * i;
+	}
+
+	return factorialNum;
 }
 
 module.exports = {
