659157cc2d783b6cd4a8ef274f3d039bc68f1f36
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-02-27 16:56:07 +0800

    Add solution to fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibNum = parseInt(num);
 
+    if(!Number.isInteger(fibNum) || fibNum < 0) {
+        return 'OOPS';
+    }
+
+    let arr = [1, 1];
+
+    for (let i = 2; i <= fibNum; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+
+    return arr[num - 1];
 }
 
 module.exports = fibonacci
