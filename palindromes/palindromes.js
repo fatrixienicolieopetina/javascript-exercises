3c9ebb2b9cca4bfb5a5718cf1479a0b16e74798b
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-02-27 16:35:19 +0800

    Added solution for palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function() {
+const palindromes = function(str) {
+    let cleanedStr = str.toLowerCase().replace(/[.,! ]/g,"");
+    let reversedStr = cleanedStr.split('').reverse().join('');
 
+    return (reversedStr == cleanedStr); 
 }
 
 module.exports = palindromes
