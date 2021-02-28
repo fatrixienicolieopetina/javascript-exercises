ca96b15fa7e1f97dbaee9a7033922ee2ff7b22ef
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-02-28 11:12:01 +0800

    Add solution for findTheOldest exercise

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let peopleWithTheirAge = [];
+    people.map(person => {
 
+        let deathYear = person.yearOfDeath == undefined ? 
+            new Date().getFullYear() : person.yearOfDeath;
+
+        let age = deathYear - person.yearOfBirth;
+        peopleWithTheirAge.push({
+            name: person.name,
+            age: age,
+        });
+    });
+
+    peopleWithTheirAge = peopleWithTheirAge.sort((p1, p2) => {
+        return p1.age > p2.age ? -1 : 1;
+    });
+
+    return peopleWithTheirAge[0];
 }
 
 module.exports = findTheOldest
